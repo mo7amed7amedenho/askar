0dbf39c068b722cfb3bb16a55938a90711e6df78
Hamedenho <mo7amed.7amedenho@gmail.com>
2024-12-10 17:05:45 +0200

    Fix Bugs

--- app/providers.tsx
+++ app/providers.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+      <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
         {children}
       </NextThemesProvider>
     </NextUIProvider>
